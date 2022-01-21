d1dca39450481e9a288e3c07bd424527e31998ec
MayPhoo-Github <mayphoowai14@gmail.com>
2022-01-21 16:56:35 +0630

    [master]add cmt for RSpec testing

--- spec/models/post_spec.rb
+++ spec/models/post_spec.rb
@@ -1,26 +1,26 @@
 require "rails_helper"
 
 RSpec.describe Post, type: :model do
-  context "validation tests" do
-    it "ensures title presence" do
-      post = Post.new(description: "lorem test lorem test lorem test", public_flag: "true", created_user_id: "7").save
-      expect(post).to eq (false)
-    end
-    it "ensures description presence" do
-      post = Post.new(title: "lorem test2", public_flag: "true", created_user_id: "7").save
-      expect(post).to eq (false)
-    end
-    it "ensures public_flag presence" do
-      post = Post.new(title: "lorem test2", description: "lorem test lorem test lorem test", created_user_id: "7").save
-      expect(post).to eq (false)
-    end
-    it "ensures created_user_id presence" do
-      post = Post.new(title: "lorem test2", description: "lorem test lorem test lorem test", public_flag: "true").save
-      expect(post).to eq (false)
-    end
-    it "should save successfully" do
-      post = Post.new(title: "lorem test2", description: "lorem test lorem test lorem test", public_flag: "true", created_user_id: "7").save
-      expect(post).to eq (true)
-    end
-  end
+  # context "validation tests" do
+  #   it "ensures title presence" do
+  #     post = Post.new(description: "lorem test lorem test lorem test", public_flag: "true", created_user_id: "7").save
+  #     expect(post).to eq (false)
+  #   end
+  #   it "ensures description presence" do
+  #     post = Post.new(title: "lorem test2", public_flag: "true", created_user_id: "7").save
+  #     expect(post).to eq (false)
+  #   end
+  #   it "ensures public_flag presence" do
+  #     post = Post.new(title: "lorem test2", description: "lorem test lorem test lorem test", created_user_id: "7").save
+  #     expect(post).to eq (false)
+  #   end
+  #   it "ensures created_user_id presence" do
+  #     post = Post.new(title: "lorem test2", description: "lorem test lorem test lorem test", public_flag: "true").save
+  #     expect(post).to eq (false)
+  #   end
+  #   it "should save successfully" do
+  #     post = Post.new(title: "lorem test2", description: "lorem test lorem test lorem test", public_flag: "true", created_user_id: "7").save
+  #     expect(post).to eq (true)
+  #   end
+  # end
 end
